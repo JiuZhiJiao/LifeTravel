4a21d5159e0e18d125a068dd8974932006e478f4
JiuZhiJiao <jiuzhijiao@gmail.com>
2020-11-04 17:40:11 +1100

    Basic Storyboard Finished

---
+++ LifeTravel/NoteListTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NoteListTableViewCell.swift
+//  LifeTravel
+//
+//  Created by JiuZhiJiao on 4/11/20.
+//  Copyright © 2020 JiuZhiJiao. All rights reserved.
+//
+
+import UIKit
+
+class NoteListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var noteDate: UILabel!
+    @IBOutlet weak var noteLocation: UILabel!
+    @IBOutlet weak var noteContent: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
